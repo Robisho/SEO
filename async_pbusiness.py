12561b60abbfbbc771ec7bb1cc2e0aaf4cb74136
Robisho <50827400+Robisho@users.noreply.github.com>
2023-05-21 10:26:19 +0800

    update file

--- async_pbusiness.py
+++ async_pbusiness.py
@@ -19,20 +19,26 @@ import socket
 DOMAIN = 'https://p-business.ru'
 
 headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
-    }
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+}
 
 
 async def get_html_code(session, url):  # получение html кода с любой стр
     async with session.get(url, headers=headers) as response:
-        response.encoding = 'utf8'
+        try:
+            response.encoding = 'utf8'
+        except Exception:
+            pass
         html_code = await response.text()
         return html_code
 
 
 async def get_byte_code(session, url):  # получение byte кода с любой стр
     async with session.get(url, headers=headers) as response:
-        response.encoding = 'utf8'
+        try:
+            response.encoding = 'utf8'
+        except Exception:
+            pass
         byte_code = await response.read()
         return byte_code
 
@@ -45,12 +51,11 @@ async def get_urls(session):
     return urls  # список всех урлов с карты сайта
 
 
-async def get_folder(session, url):
-    html_code = await get_html_code(session, url)
+async def get_folder(session, html_code, url):
     dom_tree = html.fromstring(html_code)
     folder = dom_tree.xpath('//title')[0].text  # имя папки с файлами
     if len(folder) > 1:  # если тайтл не пустой
-        folder = re.sub('\W+', ' ', folder) # удаляет спецсимволы из строки
+        folder = re.sub('\W+', ' ', folder)  # удаляет спецсимволы из строки
         if len(folder) >= 45:  # чтобы имя папки было не очень большим
             folder = folder[:45]
     else:  # если тайтл пустой
@@ -60,22 +65,21 @@ async def get_folder(session, url):
     return folder
 
 
-async def get_img_urls(session, url):  # +
-    html_code = await get_html_code(session, url)
+async def get_img_urls(session, html_code):  # +
     dom_tree = html.fromstring(html_code)
     img_links = dom_tree.xpath("//div[@class='entry']//img/@src")
     return img_links  # список всех урлов статьи, ведущих на картинки
 
 
 async def save_img(session, img_url, folder):
-    print(f'Продолжаем {folder}')
+    print(f'Продолжаем с {folder}')
     try:
         response = await get_byte_code(session, img_url)
         name = img_url.split('/')[-1]
         await asyncio.sleep(2)
         write_img(response, name, folder)
     except FileNotFoundError as err:
-        print('Картинка не скачивается', img_url)
+        print('Картинка не скачивается', img_url, err)
         pass
     except Exception as err:
         print(err, type(err))
@@ -83,50 +87,42 @@ async def save_img(session, img_url, folder):
 
 def write_img(response, name, folder):
     with open(f'{folder}/{name}', 'wb') as pic:
-            pic.write(response)
+        pic.write(response)
 
 
-async def parse_url(session, url, sem, attempts=3):  # +
-    a = attempts
-    print('-' * 30)
-    print('Парсинг', url)
-    print('Осталось попыток:', a)
-    print('-' * 30)
+async def parse_url(session, url, sem):  # +
     async with sem:
-        try:
-            await asyncio.sleep(2)
-            folder = await get_folder(session, url)
-            img_urls = await get_img_urls(session, url)
-            for img_url in img_urls:
-                await save_img(session, img_url, folder) # сохранение img в файл
+        for i in range(3):
+            print('-' * 30)
+            print(f'Парсинг {i+1}', url)
+            try:
+                await asyncio.sleep(2)
+                html_code = await get_html_code(session, url)
+                folder = await get_folder(session, html_code, url)
+                img_urls = await get_img_urls(session, html_code)
+                for img_url in img_urls:
+                    # сохранение img в файл
+                    await save_img(session, img_url, folder)
 
-        except ConnectionError as e:
-            print('Ошибка на ссылке', url)
-            if a > 1:
-                a -= 1
-                await asyncio.sleep(30)
-                await parse_url(session, url, sem, a)
+            except ConnectionError as e:
+                print('Ошибка на ссылке', url, e)
+                await asyncio.sleep(10)
 
-        except aiohttp.client_exceptions.ServerDisconnectedError as err:
-            print('Сервер разорвал коннект', url)
-            if a > 1:
-                a -= 1
+            except aiohttp.client_exceptions.ServerDisconnectedError as err:
+                print('Сервер разорвал коннект', url, err)
                 await asyncio.sleep(30)
-                await parse_url(session, url, sem, a)
 
-        except aiohttp.client_exceptions.ClientConnectorError as err:
-            print('Превышен таймаут семафора', url)
-            if a > 1:
-                a -= 1
+            except aiohttp.client_exceptions.ClientConnectorError as err:
+                print('Превышен таймаут семафора', url, err)
                 await asyncio.sleep(30)
-                await parse_url(session, url, sem, a)
 
-        except asyncio.exceptions.TimeoutError as err:
-            print('TimeoutError', url)
-            if a > 1:
-                a -= 1
+            except asyncio.exceptions.TimeoutError as err:
+                print('TimeoutError', url, err)
                 await asyncio.sleep(30)
-                await parse_url(session, url, sem, a)
+
+            except Exception as ex:
+                print('Ошибка', ex)
+                await asyncio.sleep(10)
 
 
 async def run():  # +
